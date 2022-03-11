c75704868451b79d92335d5fbc1a7df91c1b802b
Tianchou Gong <tianchougong@gmail.com>
2022-03-10 23:38:37 -0500

    API added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 var userFormEl = document.querySelector('#user-form');
-var nameInputEl = document.querySelector('#name');
+var PlayernameInputEl = document.querySelector('#playername');
 //var teamNameInputEl = document.querySelector('#teamname');
 var nameContainerEl = document.querySelector('#name-container');
 //var teamContainerEl = document.querySelector('#team-container');
@@ -72,3 +72,30 @@ var getPlayerStats = function (name){
 
 
 userFormEl.addEventListener('submit',formSubmitHandler);
+}
+
+
+
+
+fetch("https://sportscore1.p.rapidapi.com/sports/1/teams?page=1", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "sportscore1.p.rapidapi.com",
+		"x-rapidapi-key": "b15a4fad3fmshcfa975c0b8052ecp142c44jsnf494d29e6230"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
+
+
+fetch('https://gnews.io/api/v4/search?q=example&token=761d21cd3ee94c2d0b71ba65f6b3f22d')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    });
